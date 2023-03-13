470ff1aa24b83b8733cb48fb3d722aff0a9c1791
e-oladipo <127542773+e-oladipo@users.noreply.github.com>
2023-03-13 08:58:38 -0600

    Update streamlit_app.py
    Fixed type

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
-streamlit.Header('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamli.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-boiled Free-Range Egg')
