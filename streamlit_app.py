532bfaa9a69c570759675c7348c902a5e11fa799
e-oladipo <127542773+e-oladipo@users.noreply.github.com>
2023-03-14 08:28:30 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ streamlit.header('Breakfast Menu')
 
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-boiled Free-Range Egg')
+streamlit.text('Hard-boiled Egg')
